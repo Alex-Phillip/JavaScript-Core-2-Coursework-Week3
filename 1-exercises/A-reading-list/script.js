42eea49f868016afa85908e819064c2ac8bacb8d
Alex Phillip <alexjoelphillip@gmail.com>
2022-09-28 16:22:01 +0100

    attempted exercises A reading list

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,6 +1,35 @@
 function readingList(books) {
-  // Write your code here...
-}
+  //"content" div link
+  let contentDiv = document.getElementById("content");
+  //create new "p" element
+  let paragraph = document.createElement("p");
+  //create new "img" element
+  let image = document.createElement("img");
+  //create new "ul" element
+  let unorderedList = document.createElement("ul");
+  //create new "li" element
+  let list = document.createElement("li");
+  
+  //loop through objects inside "books" array
+  for (let i = 0; i < books.length; i++) {
+  //set text content of "p" element
+    paragraph.innerText = `${books[i].title} by ${books[i].author}`;
+  //append text content to "li" element
+    list.appendChild(paragraph);
+  //set content of "img" element
+    image.src = `${books[i].bookCoverImage}`;
+  //append image content to "li" element
+    list.appendChild(image);
+  //if statement to check if book has been read
+    (books[i].alreadyRead === true)
+    ? list.style.backgroundColor = "green"
+    : list.style.backgroundColor = "red";
+  //append "li" element to "ul" element
+    unorderedList.appendChild(list);
+  //append "ul" element to "content" div
+    contentDiv.appendChild(unorderedList);
+    }
+  };
 
 const books = [
   {
